1d7a69e21bccd3b9342abc4f082681714e45fffe
dollarkillerx <adapawang@gmail.com>
2020-04-25 18:02:32 +0800

    add

--- README.md
+++ README.md
@@ -1,26 +1,138 @@
-# Proficient-in-rust
-Proficient in rust 一个月精通rust
+# 编程基础
+``` 
+let x = 5;
+    let x = x + 1;
+    let x = x * 2;
 
-### 一些约定
-- 每一个阶段为 一改 分支  
-- master 分支为 index
+    let mut c = 12;
+    let mut c = "asdasd";
+    /**
+    *   let x = 1;
+    *   let x = x + 6;  // 对x进行重新计算 变量仍然复用
+    *
+    *   let mut c = 12;
+    *   let mut c = "asdasd";  // 创建新的变量 复用来 c这个名字
+    */
+    println!("The value of x is: {}",x);
+```
 
-### 学习目标
-- rust 基本语法
-- socks5 实现
-- arm  mips 等IOT架构实现
+### 数据类型
+rust 每一个值都属于某一个数据类型
+- 标量
+- 复合
+``` 
+# 编译时 必须指定 类型  (rust编译器会自动推导)
+let guess: u32 = "42".parse().expect("Not a number!");
+parse() 是类型转换   需要告诉它要转换为的数据类型
+```
 
-### 环境
-- idea2020.1 
-- Manjaro Linux
-- rust 1.43.0 (4fb7144ed 2020-04-20)
+|len|有符号|无符号|
+|-|-|-|
+| 8-bit | i8 | u8 |
+| 16-bit | i16 | u16 |
+| 32-bit | i32 | u32 |
+| 64-bit | i64 | u64 |
+| arch | isize | usize| 
 
+isize usize 基于系统 位数 改变
 
-### 分支
-- master  index
-- day1    helloworld cargo包管理工具
-- day2    变量 常量 流程控制  循环 逻辑  (猜数字)
-- day3    语言基础
+### 原生复合类型
+- tuple 元祖 ` let tup:(i32,f64,u8) = (500,6.3,1);`
+- array 数组  `let a = [1,2,3,5];`
+``` 
+tuple:
+    let tup:(i32,f64,u8) = (500,6.3,1);
+    let (x,y,z) = tup; //  解构
+    println!("x: {} y: {} z: {}",x,y,z);
+    println!("x: {} y: {} z: {}",tup.0,tup.1,tup.2);
 
-### Cargo
-https://lug.ustc.edu.cn/wiki/mirrors/help/rust-crates
+array:
+    let a = [0,1,2,3,4,5]; // a: [i32;6]
+
+    // 数组 在 stack 上
+    println!("a[0]: {}",a[0]);
+
+    for index in 0..a.len() {
+        println!("index is: {} &value is: {}",index,a[index]);
+    }
+
+    println!("Ii: {:?}",a);
+```
+
+### 函数
+- 匿名函数
+``` 
+    // 匿名函数
+    let hello:fn(i:String) = |name| {
+        println!("Hello {}",name);
+    };
+
+    let mut name = String::new();
+    println!("Please input name: ");
+    io::stdin().read_line(&mut name).expect("蛤");
+
+    hello(name);
+```
+- 宏的一点用
+```
+    let x = 5;
+    let y = {  // 宏表达式
+      let x = 3;
+        x+1
+    };
+
+    println!("X: {} y: {}",x,y);
+    // x:5 y:4
+
+    // 返回值
+    fn five() -> i32 {
+        5
+    }
+
+    fn five() -> i32 {
+        return 5;
+    }
+```
+
+###  有趣的变量声明
+``` 
+    let number = if condition { // 哈哈
+        5
+    }else {
+        4
+    };
+```
+
+###  循环
+loop:
+``` 
+let mut c = 0;
+let a = loop {
+    c += 1;
+    if c == 200 {
+        break c;
+    }
+};
+```
+for
+``` 
+    for index in 0..10 {
+        println!("{}",index);
+    }
+
+    let a = [10,20,30,40,50];
+    for item in a.iter() {
+        println!("this val is: {}",item);
+    }
+    // this val is: 10
+
+    for index in 0..a.len() {
+        println!("idx: {} val: {}",index,a[index]);
+    }
+
+    println!();
+
+    for num in(1..4).rev() { // .rev 反转数组
+        println!("{}!",num);
+    }
+```
--- hellorust/src/main.rs
+++ hellorust/src/main.rs
@@ -1,3 +1,154 @@
+use std::io;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // definition();
+    // pc();
+    // tuple();
+    // list();
+
+    // function();
+    // fun2();
+    // ifElse();
+
+    // v2();
+    // lop();
+    for1();
+}
+
+fn definition() {
+    let mut x = 5; // mut 为可变的  (类型 和 数值 可变)
+    let b = 12;    // 默认定义为 不可变
+    println!("The value of x is {}",x);
+
+    // 定义常量
+    const MAX_POINTS: u32 = 100_000; // 定义 100,000 rust 支持_提高可读性
+}
+
+fn pc() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
+    let mut c = 12;
+    let mut c = "asdasd";
+    /**
+    *   let x = 1;
+    *   let x = x + 6;  // 对x进行重新计算 变量仍然复用
+    *
+    *   let mut c = 12;
+    *   let mut c = "asdasd";  // 创建新的变量 复用来 c这个名字
+    * */
+    println!("The value of x is: {}",x);
+}
+
+fn tuple() {
+    let tup:(i32,f64,u8) = (500,6.3,1);
+    let (x,y,z) = tup; //  解构
+    println!("x: {} y: {} z: {}",x,y,z);
+    println!("x: {} y: {} z: {}",tup.0,tup.1,tup.2);
+}
+
+fn list() {
+    let a = [0,1,2,3,4,5]; // a: [i32;6]
+
+    // 数组 在 stack 上
+    println!("a[0]: {}",a[0]);
+
+    for index in 0..a.len() {
+        println!("index is: {} &value is: {}",index,a[index]);
+    }
+
+    println!("Ii: {:?}",a);
+}
+
+fn function() {
+    // 匿名函数
+    let hello:fn(i:String) = |name| {
+        println!("Hello {}",name);
+    };
+
+    let mut name = String::new();
+    println!("Please input name: ");
+    io::stdin().read_line(&mut name).expect("蛤");
+
+    hello(name);
+}
+
+fn fun2() {
+    let x = 5;
+    let y = {  // 宏表达式
+      let x = 3;
+        x+1
+    };
+
+    println!("X: {} y: {}",x,y);
+    // x:5 y:4
+
+    println!("a: {}",five());
+}
+
+fn five() -> i32 {
+    // return 5;
+    5
+}
+
+fn ifElse() {
+   let c:fn(i:u8) = |i|{
+       if i >100 {
+           println!(">100")
+       }else if i>50 {
+           println!("> 50")
+       }else {
+           print!("0.0");
+       }
+   };
+
+    c(1);
+    c(20);
+    c(100);
+}
+
+fn v2() {
+    let condition = true;
+    let number = if condition { // 哈哈
+        5
+    }else {
+        4
+    };
+
+    println!("Number: {}",number);
+}
+
+fn lop() {
+    let mut counter = 0;
+    let result = loop {
+        counter +=1;
+        //  当前线程 sleep
+        thread::sleep(Duration::from_millis(200));
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result,20);
+}
+
+fn for1() {
+    let a = [10,20,30,40,50];
+    for item in a.iter() {
+        println!("this val is: {}",item);
+    }
+    // this val is: 10
+
+    for index in 0..a.len() {
+        println!("idx: {} val: {}",index,a[index]);
+    }
+
+    println!();
+
+    for num in(1..4).rev() { // .rev 反转数组
+        println!("{}!",num);
+    }
 }

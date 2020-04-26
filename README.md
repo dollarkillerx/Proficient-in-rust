456d4a13818a2e76c1ddc5efc2fefdc18735cd0d
dollarkillerx <adapawang@gmail.com>
2020-04-26 22:46:18 +0800

    add match enum

--- README.md
+++ README.md
@@ -1,43 +1,76 @@
-# Proficient-in-rust
-Proficient in rust 一个月精通rust
+# 枚举和模式匹配
 
-### 一些约定
-- 每一个阶段为 一改 分支  
-- master 分支为 index
+### 简单枚举
+``` 
+fn t1() {
+    let v1 = IpAddr{
+        kind:IpAddKind::V6(String::from("::1")),
+    };
+    let v2 = IpAddr{
+        kind:IpAddKind::V4(8,8,8,8),
+    };
 
-### 学习目标
-- rust 基本语法
-- socks5 实现
-- arm  mips 等IOT架构实现
+    println!("V1: {:#?}",v1);
+    println!("V2: {:#?}",v2);
+}
 
-### 环境
-- idea2020.1 
-- Manjaro Linux
-- rust 1.43.0 (4fb7144ed 2020-04-20)
+#[derive(Debug)]
+enum IpAddKind{
+    V4(u8,u8,u8,u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddKind,
+}
+```
+
+### 经典用法
+``` 
+// 经典用法
+enum Message {
+    Quit,
+    Move{x: i32,y:i32 },
+    Write(String),
+    Change(i32,i32,u32),
+}
+
+impl Message {
+    fn print(&self) {
+        match *self {
+            Message::Quit => println!("QUit"),
+            Message::Move{x,y} => println!("Move x = {} y = {}",x,y),
+            Message::Change(a,b,c) => println!("Change a = {} b = {} c ={} ",a,b,c),
+            _ => println!("All"),
+            // Message::Write(&s) => println!("s: {}",s), String 这个需要所有权 这里 取的啥* 无法打印
+        }
+    }
+}
+```
 
 
-### 分支
-- master  index
-- day1    helloworld cargo包管理工具
-- day2    猜数字 demo
-- day3    语言基础 (变量 常量 流程控制  循环 逻辑  函数)
-- day4    所有权  (所有权 借用 引用 堆栈 拷贝) `在任意给定时间，要么 只能有一个可变引用，要么 只能有多个不可变引用。` * 非常重要  
-- day5    Slice 类型 (这个和go差不多都是{point,len,cap} 但是切片颗粒度有区别)
-- day6    结构体
-- day7    枚举和模式匹配
-- day8    包管理
-- day9    集合
-- day10   错误
-- day11   泛形,trait 与 生命周期
-- day12   测试
-- day13   一个简单的小demo
-- day14   函数和闭包
-- day15   Cargo
-- day16   智能指针
-- day17   无畏并发
-- day18   面向对象
-- day19   模式与结构
-- day20   高级特性
-- day22   web实战(虽然web很无趣)
-### Cargo
-https://lug.ustc.edu.cn/wiki/mirrors/help/rust-crates
+### Option 标准类型
+``` 
+fn t2() {
+    let some_number = Some(5);
+    let some_string = Some(String::from("A String"));
+    let absent_number:Option<i32> = None;
+
+    let x:i32 =5;
+    let y:Option<i32> = Some(5);
+    let mut tmp = 0;
+    match y {
+        Some(i) => tmp = i,
+        None => println!("Do nothing"),
+    }
+
+    if let Some(i) = some_number {
+        println!("I: {}",i);
+    }else {
+        println!("is null");
+    }
+    println!("Tmp: {}",tmp);
+    println!("Hello World");
+}
+```
--- hellorust/src/main.rs
+++ hellorust/src/main.rs
@@ -1,32 +1,93 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
 fn main() {
-    println!("猜猜看!!!");
-    let caiCai = rand::thread_rng().gen_range(0,100);
-   loop {
-       print!("输入你所猜所想: ");
-       let mut input = String::new();
-
-       io::stdin().read_line(&mut input).
-           expect("蛤 你输入的啥子???");
-
-       // to uint32
-       let inputUint: u32 = match input.trim().parse() {
-           Ok(num) => num,
-           Err(_) => continue,
-       };
-
-       println!("you Input: {}",inputUint);
-
-       match inputUint.cmp(&caiCai) {
-           Ordering::Less => println!("Less"),
-           Ordering::Equal => {
-               println!("Equal");
-               break;
-           },
-           Ordering::Greater => println!("Greater"),
-       }
-
-   }
+    // t1();
+    t2();
 }
+
+// enum Option<T> { // <T> 泛形
+//     Some(T),
+//     None,
+// }
+
+fn t2() {
+    let some_number = Some(5);
+    let some_string = Some(String::from("A String"));
+    let absent_number:Option<i32> = None;
+
+    let x:i32 =5;
+    let y:Option<i32> = Some(5);
+    let mut tmp = 0;
+    match y {
+        Some(i) => tmp = i,
+        None => println!("Do nothing"),
+    }
+
+    if let Some(i) = some_number {
+        println!("I: {}",i);
+    }else {
+        println!("is null");
+    }
+    println!("Tmp: {}",tmp);
+    println!("Hello World");
+}
+
+fn t1() {
+    let v1 = IpAddr{
+        kind:IpAddKind::V6(String::from("::1")),
+    };
+    let v2 = IpAddr{
+        kind:IpAddKind::V4(8,8,8,8),
+    };
+
+    println!("V1: {:#?}",v1);
+    println!("V2: {:#?}",v2);
+    t1_t(&v1.kind);
+    println!("V1: {:#?}",v1);
+}
+
+fn t1_t(ip: &IpAddKind) {
+    match *ip {
+        IpAddKind::V4(_,_,_,_) => println!("v4"),
+        IpAddKind::V6(_) => println!("v6"),
+        _ => println!("ha"),
+    }
+}
+
+#[derive(Debug)]
+enum IpAddKind{
+    V4(u8,u8,u8,u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddKind,
+}
+
+// 经典用法
+enum Message {
+    Quit,
+    Move{x: i32,y:i32 },
+    Write(String),
+    Change(i32,i32,u32),
+}
+
+impl Message {
+    fn print(&self) {
+        match *self {
+            Message::Quit => println!("QUit"),
+            Message::Move{x,y} => println!("Move x = {} y = {}",x,y),
+            Message::Change(a,b,c) => println!("Change a = {} b = {} c ={} ",a,b,c),
+            _ => println!("All"),
+            // Message::Write(&s) => println!("s: {}",s),
+        }
+    }
+}
+
+// 等同于
+// strcut QuitMessage; // 类单元结构体
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String)
+// struct Change(i32,i32,u32)

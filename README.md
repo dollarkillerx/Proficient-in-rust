789f61b684375e2747f225b7d2820c143ffc5363
dollarkillerx <adapawang@gmail.com>
2020-04-27 23:46:29 +0800

    add

--- README.md
+++ README.md
@@ -1,43 +1,115 @@
-# Proficient-in-rust
-Proficient in rust 一个月精通rust
+# 集合
+- Vector
+- String
+- HashMap
 
-### 一些约定
-- 每一个阶段为 一改 分支  
-- master 分支为 index
+### Vector
+``` 
+// 声明 vec i32 可变数组
+let v: Vec<i32> = Vec::new();
 
-### 学习目标
-- rust 基本语法
-- socks5 实现
-- arm  mips 等IOT架构实现
+let c = vec![1,2,3]; // 使用宏申明
 
-### 环境
-- idea2020.1 
-- Manjaro Linux
-- rust 1.43.0 (4fb7144ed 2020-04-20)
+let mut v = Vec::new();
+v.push(5);
+v.push(4);
+v.push(3);
+v.push(2);
+v.push(1);
+v.push(0); // 添加值
+// 当v 离开作用域时  就会被丢弃
+
+// 依次读取
+let third: &i32 = &v[2];
+println!("this third element is {}",third);
+
+match v.get(5) {
+Some(c) => println!("this third element is {}",c),
+None => println!("this is no third element."),
+}
+```
+
+### for 
+``` 
+for i in &mut v {
+    *i += 50;
+}
+
+for i in &v {
+    println!("A: {}",i);
+}
+
+ enum Spc {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+let row = vec![
+    Spc::Int(3),
+    Spc::Text(String::from("blue")),
+    Spc::Float(10.2),
+];
+
+for i in &row {
+    match i {
+        Spc::Int(x) => println!("x:{}",x),
+        Spc::Float(f) => println!("f: {}",f),
+        Spc::Text(str) => println!("Str: {}",str),
+        _ => println!("None"),
+    }
+}
+```
+
+### String
+``` 
+    let mut z0 = String::new();
+    z0.push_str("hello");
+
+    let s1 = String::from("init some thing");
+    println!("s1: {}",s1);
+
+    let mut he = "hello rust".to_string();
+    he.push_str(&s1);
+    println!("{}",he);
+    println!("{}",s1);
+
+    let ss = he + &s1; // 注意用 + he 的所有权会被移交
+    println!("{}",ss);
+
+    let s11 = String::from("s11");
+    let s22 = String::from("s22");
+    let s33 = String::from("s22");
+    let s44 = format!("{} - {} - {} ",s11,s22,s33);
+    println!("{}",s44);
+
+    let s44 = 123;
+    println!("{}",s44);
+```
+遍历
+``` 
+    let s44 = String::from("你好RUST");
+    for s in s44.bytes() { // 123 228 189 ...
+        println!("{}",s);
+    }
+
+    for s in s44.chars() {  //你 好 R U S T 
+        println!("{}",s);
+    }
+```
 
 
-### 分支
-- master  index
-- day1    helloworld cargo包管理工具
-- day2    猜数字 demo
-- day3    语言基础 (变量 常量 流程控制  循环 逻辑  函数)
-- day4    所有权  (所有权 借用 引用 堆栈 拷贝) `在任意给定时间，要么 只能有一个可变引用，要么 只能有多个不可变引用。` * 非常重要  
-- day5    Slice 类型 (这个和go差不多都是{point,len,cap} 但是切片颗粒度有区别)
-- day6    结构体
-- day7    枚举和模式匹配
-- day8    包管理
-- day9    集合
-- day10   错误
-- day11   泛形,trait 与 生命周期
-- day12   测试
-- day13   一个简单的小demo
-- day14   函数和闭包
-- day15   Cargo
-- day16   智能指针
-- day17   无畏并发
-- day18   面向对象
-- day19   模式与结构
-- day20   高级特性
-- day22   web实战(虽然web很无趣)
-### Cargo
-https://lug.ustc.edu.cn/wiki/mirrors/help/rust-crates
+### HashMap
+创建
+``` 
+方法1：
+    let mut sores = HashMap::new();
+
+方法2：
+    let keys = vec![String::from("Blue"),String::from("Red")];
+    let values = vec![20,10];
+    
+    let scores:HashMap<String,i32> = keys.iter().zip(values.iter()).collect();
+    let scores:HashMap<_,_> = keys.iter().zip(values.iter()).collect();
+
+```
--- hellorust/src/main.rs
+++ hellorust/src/main.rs
@@ -1,32 +1,148 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+use std::collections::HashMap;
 fn main() {
-    println!("猜猜看!!!");
-    let caiCai = rand::thread_rng().gen_range(0,100);
-   loop {
-       print!("输入你所猜所想: ");
-       let mut input = String::new();
+    test1();
+    test2();
+    test3();
+    test_str_1();
+    test_hash_1();
+}
 
-       io::stdin().read_line(&mut input).
-           expect("蛤 你输入的啥子???");
+fn test_hash_1() {
+    println!();
+    println!("=== Hash ===");
 
-       // to uint32
-       let inputUint: u32 = match input.trim().parse() {
-           Ok(num) => num,
-           Err(_) => continue,
-       };
+    // 创建
+    let mut sores = HashMap::new();
+    if let None = sores.insert("H",12) {
+        println!("Success");
+    }
 
-       println!("you Input: {}",inputUint);
+    let keys = vec![String::from("Blue"),String::from("Red")];
+    let values= vec![20,10];
 
-       match inputUint.cmp(&caiCai) {
-           Ordering::Less => println!("Less"),
-           Ordering::Equal => {
-               println!("Equal");
-               break;
-           },
-           Ordering::Greater => println!("Greater"),
-       }
+    let scores:HashMap<String,i32> = keys.iter().zip(values.iter()).collect();
+    // let scores:HashMap<_,_> = keys.iter().zip(values.iter()).collect();
 
-   }
+}
+
+fn test_str_1() {
+    println!();
+    println!("=== Str ===");
+
+    let mut z0 = String::new();
+    z0.push_str("hello");
+
+    let s1 = String::from("init some thing");
+    println!("s1: {}",s1);
+
+    let mut he = "hello rust".to_string();
+    he.push_str(&s1);
+    println!("{}",he);
+    println!("{}",s1);
+
+    he.push(' ');
+    he.push('_');
+    he.push('a');
+    he.push('b');
+    he.push('c');
+    he.push('_');
+
+    println!("{}",he);
+
+    let ss = he + &s1; // 注意用 + he 的所有权会被移交
+    println!("{}",ss);
+
+    let s11 = String::from("s11");
+    let s22 = String::from("s22");
+    let s33 = String::from("s22");
+    let s44 = format!("{} - {} - {} ",s11,s22,s33);
+    println!("{}",s44);
+
+    let s44 = 123;
+    println!("{}",s44);
+
+    let s44 = String::from("你好RUST");
+    for s in s44.bytes() {
+        println!("{}",s);
+    }
+
+    for s in s44.chars() {
+        println!("{}",s);
+    }
+}
+
+fn test3() {
+    println!();
+    println!("====================");
+
+    let mut a = Vec::new();
+    a.push("a");
+    a.push("b");
+    a.push("c");
+
+    a.pop();
+    a.pop();
+
+
+    println!("{}",a.len());
+
+    // a.push("b");
+}
+
+fn test2() {
+    enum Spc {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        Spc::Int(3),
+        Spc::Text(String::from("blue")),
+        Spc::Float(10.2),
+    ];
+
+    for i in &row {
+        match i {
+            Spc::Int(x) => println!("x:{}",x),
+            Spc::Float(f) => println!("f: {}",f),
+            Spc::Text(str) => println!("Str: {}",str),
+        }
+    }
+
+
+}
+
+fn test1() {
+    // 声明 vec i32 可变数组
+    // let v: Vec<i32> = Vec::new();
+
+    // let c = vec![1,2,3]; // 使用宏申明
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(4);
+    v.push(3);
+    v.push(2);
+    v.push(1);
+    v.push(0); // 添加值
+    // 当v 离开作用域时  就会被丢弃
+
+    // 依次读取
+    let third: &i32 = &v[2];
+    println!("this third element is {}",third);
+
+    match v.get(5) {
+        Some(c) => println!("this third element is {}",c),
+        None => println!("this is no third element."),
+    }
+
+    for i in &mut v {
+        *i += 50;
+    }
+
+    for i in &v {
+        println!("A: {}",i);
+    }
+
 }
